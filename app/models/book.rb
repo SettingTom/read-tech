6d497eadb5124c7fd8f9c39f65b0eb07e7a8a22d
SettingTom <tnbt0277@gmail.com>
2022-08-22 22:50:26 +0900

    bookバリデーション設定2

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,11 @@ class Book < ApplicationRecord
   validates :book_name, presence: true
 
   belongs_to :user
+  has_many :mentalmaps
+  has_many :curiosity_gaps
+  has_many :authors
+  has_many :resumes
+  has_many :visualizings
+  has_many :summarizings
+  has_many :questions
 end
