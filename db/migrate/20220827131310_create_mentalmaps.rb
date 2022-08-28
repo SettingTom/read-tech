3c9140ca13af2b3ed409cb9a4d2f9066134293cf
SettingTom <tnbt0277@gmail.com>
2022-08-28 22:11:28 +0900

    mentalmap作成ページの作成

--- db/migrate/20220827131310_create_mentalmaps.rb
+++ db/migrate/20220827131310_create_mentalmaps.rb
@@ -1,10 +1,10 @@
 class CreateMentalmaps < ActiveRecord::Migration[6.0]
   def change
     create_table :mentalmaps do |t|
-      t.text       :why_text    , null: false
-      t.text       :what_text    , null: false
-      t.text       :how_text    , null: false
-      t.references :book , null: false, foreign_key: true
+      t.text       :why_text  , null: false
+      t.text       :what_text , null: false
+      t.text       :how_text  , null: false
+      t.references :book      , null: false, foreign_key: true
       t.timestamps
     end
   end
