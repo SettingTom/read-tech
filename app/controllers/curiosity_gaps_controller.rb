dcbb05328dea092d89e2d724dc91257107d935bc
SettingTom <tnbt0277@gmail.com>
2022-09-07 07:22:54 +0900

    quriosity_gapsコントローラーnew,createアクション記述

--- app/controllers/curiosity_gaps_controller.rb
+++ app/controllers/curiosity_gaps_controller.rb
@@ -1,2 +1,21 @@
 class CuriosityGapsController < ApplicationController
+  def new
+    @quriosity_gap = CuriosityGap.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    @quriosity_gap = CuriosityGap.new(quriosity_gap_params)
+    if @quriosity_gap.save
+      redirect_to book_path(params[:book_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quriosity_gap_params
+    params.permit(:already_text, :unknown_text).merge(book_id: params[:book_id])
+  end
 end
