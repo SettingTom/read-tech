8ce61625faefc0cb622935ae9436034b68e01940
SettingTom <tnbt0277@gmail.com>
2022-09-10 22:30:15 +0900

    curiosity_gap作成ページ遷移機能実装、誤字修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :books, only: [:index, :new, :create, :show] do
     resource :mentalmaps, only: [:new, :create]
-    resource :quriosity_gaps, only: [:new, :create]
+    resource :curiosity_gaps, only: [:new, :create]
   end
 end
