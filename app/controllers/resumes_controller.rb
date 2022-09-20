66d5f9572c0419d52b6bab98ef0087f335e35147
SettingTom <tnbt0277@gmail.com>
2022-09-20 22:16:28 +0900

    resumesコントローラーnew,createアクション記述

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,21 @@
 class ResumesController < ApplicationController
+  def new
+    @resume = Resume.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    @resume = Resume.new(resume_params)
+    if @resume.save
+      redirect_to book_path(params[:book_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def resume_params
+    params.permit(:resume_chapter, :resume_motive_text).merge(book_id: params[:book_id])
+  end
 end
