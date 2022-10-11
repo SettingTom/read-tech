98ab8b28232e7bc4fd0fa6f0cfb9fbc55c439fdd
SettingTom <tnbt0277@gmail.com>
2022-10-11 12:11:31 +0900

    visualizings new,createアクション記述

--- app/controllers/visualizings_controller.rb
+++ app/controllers/visualizings_controller.rb
@@ -1,2 +1,21 @@
 class VisualizingsController < ApplicationController
+  def new
+    @visualizing = Visualizing.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    @visualizing = Vvisualizing.new(visualizing_params)
+    if @visualizing.save
+      redirect_to book_path(params[:book_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def visualizing_params
+    params.permit(:premise, :explanation, :conclusion).merge(book_id: params[:book_id])
+  end
 end
