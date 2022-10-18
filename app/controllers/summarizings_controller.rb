dec41a1e01965861d64c9645f46966b0aef6381f
SettingTom <tnbt0277@gmail.com>
2022-10-18 18:58:16 +0900

    summarizing new,createアクション記述

--- app/controllers/summarizings_controller.rb
+++ app/controllers/summarizings_controller.rb
@@ -1,2 +1,21 @@
 class SummarizingsController < ApplicationController
+  def new
+    @summarizing = Summarizing.new
+    @book = Book.find(params[:book_id])
+  end
+
+  def create
+    @summarizing = Summarizing.new(summarizing_params)
+    if @summarizing.save
+      redirect_to book_path(params[:book_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def summarizing_params
+    params.permit(:summarizing_chapter, :summarizing_text).merge(book_id: params[:book_id])
+  end
 end
