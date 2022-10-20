9144d643626d66d6bbf2f87f10f44a9cf0c993c6
SettingTom <tnbt0277@gmail.com>
2022-10-20 23:03:41 +0900

    questioningsコントローラ作成

---
+++ app/controllers/questionings_controller.rb
@@ -0,0 +1,2 @@
+class QuestioningsController < ApplicationController
+end
---
+++ test/controllers/questionings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuestioningsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
